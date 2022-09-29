db7a1e34a82eb06fd208d62f6bf893214f3308d7
codylellis <codylee.ellis@gmail.com>
2022-09-29 10:29:48 -0500

    made script for normal sms

--- gitpush.sh
+++ gitpush.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter git comment: " com
 
-git add gw_bulk.py gitpush.sh README.md
+git add gw_bulk_sms.py gw_bulk_mdm.py gitpush.sh README.md
 git commit -m "$com"
 
 git push -u origin main
